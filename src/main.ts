9c562006bc7e58a93a3b30dfc87568097c2604d4
Tobias Wilker <twilker@phoenixcontact.com>
2023-07-04 07:04:49 +0200

    Add global prefix for rewrites

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Logger } from '@nestjs/common'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true })
+    app.setGlobalPrefix('api')
     await app.listen(parseInt(process.env.PORT) || 3000)
     Logger.log(`Application is running on: ${await app.getUrl()}`)
 }
